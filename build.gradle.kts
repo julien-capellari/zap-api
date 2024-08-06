63adea478e86968aa7078f788fbbfcc335534b8c
Julien Capellari <julien.capellari@neoxia.com>
2024-08-06 20:22:36 +0200

    chore: upload coverage

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
+    id("io.spring.dependency-management") version "1.1.6"
     id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
     id("org.springframework.boot") version "3.3.2"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("org.jetbrains.kotlinx.kover") version "0.8.3"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
 }
